4de9d5f7ee6aec88f000f44458428713a834ab99
yu <823947488@qq.com>
2018-10-02 13:27:26 +0800

    将mysql配置配到配置文件中

--- douban/douban/pipelines.py
+++ douban/douban/pipelines.py
@@ -5,24 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
+from douban.settings import *
+
 
 class DoubanPipeline(object):
-
-
     def __init__(self):
-        self.client=pymysql.connect(
-            host="127.0.0.1",
-            port=3306,
-            user="root",
-            passwd="123",
-            db="scrapy",
-            charset="utf8"
+        self.client = pymysql.connect(
+            host=MYSQL_HOST,
+            port=MYSQL_PORT,
+            user=MYSQL_USER,
+            passwd=MYSQL_PASSWD,
+            db=MYSQL_DBNAME,
+            charset=MYSQL_CHARSET
         )
-        self.cur= self.client.cursor()
+        self.cur = self.client.cursor()
 
     def process_item(self, item, spider):
         sql = "insert into douban250 values(%s,%s,%s,%s,%s,%s)"
-        data=(item["number"],item["name"],item["introduce"],item["star"],item["evaluate"],item["describe"]);
-        self.cur.execute(sql,data)
+        data = (item["number"], item["name"], item["introduce"], item["star"], item["evaluate"], item["describe"]);
+        self.cur.execute(sql, data)
         self.client.commit()
         return item
